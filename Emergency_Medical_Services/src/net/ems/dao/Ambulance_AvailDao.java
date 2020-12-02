a5f800d4bdd53a32a71448532202683129a30587
AYUSH1382.github <57244592+AYUSH1382@users.noreply.github.com>
2020-12-02 13:11:58 +0530

    Update Ambulance_AvailDao.java

--- Emergency_Medical_Services/src/net/ems/dao/Ambulance_AvailDao.java
+++ Emergency_Medical_Services/src/net/ems/dao/Ambulance_AvailDao.java
@@ -7,55 +7,22 @@ import java.sql.SQLException;
 
 import net.ems.model.Avail;
 import net.ems.utils.JDBCUtils;
-//import net.ems.model.Register;
 
-//public class Ambulance_AvailDao {
-//	public int registeravail(Avail employee) throws ClassNotFoundException {
-////		String INSERT_USERS_SQL = "INSERT INTO avail"
-////				+ "  ( name, phone_no, pick_up, drop, vehicle_type) VALUES "
-////				+ " (?, ?, ?, ?, ?);";
-//		String INSERT_USERS_SQL = "INSERT INTO avail"
-//				+ "  (name, phone_no, pick_up, drop_loc, vehicle_type ) VALUES "
-//				+ " (?, ?, ?, ?, ?);";
-//
-//		int result = 0;
-//		try (Connection connection = JDBCUtils.getConnection();
-//				// Step 2:Create a statement using connection object
-//				PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
-//			preparedStatement.setString(1, employee.getName());
-//			preparedStatement.setString(2, employee.getPhoneno());
-//			preparedStatement.setString(3, employee.getPickup());
-//			preparedStatement.setString(4, employee.getDrop());
-//			preparedStatement.setString(5, employee.getVehicle_type());
-//			
-//			
-//
-//			System.out.println(preparedStatement);
-//			// Step 3: Execute the query or update query
-//			result = preparedStatement.executeUpdate();
-//
-//		} catch (SQLException e) {
-//			// process sql exception
-//			JDBCUtils.printSQLException(e);
-//		}
-//		return result;
-//	}
-//}
 public class Ambulance_AvailDao {
-	public int registerAvail(Avail employee) throws ClassNotFoundException {
-		String INSERT_USERS_SQL = "INSERT INTO avail"
+	public int registerAvail(Avail customer) throws ClassNotFoundException {
+		String INSERT_BOOK_SQL = "INSERT INTO avail"
 				+ "  (name, phone_no, pick_up, drop_loc, vehicle_type ) VALUES "
 				+ " (?, ?, ?, ?, ?);";
 
 		int result = 0;
 		try (Connection connection = JDBCUtils.getConnection();
 				// Step 2:Create a statement using connection object
-				PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
-			preparedStatement.setString(1, employee.getName());
-			preparedStatement.setString(2, employee.getPhoneno());
-			preparedStatement.setString(3, employee.getPickup());
-			preparedStatement.setString(4, employee.getDrop());
-			preparedStatement.setString(5, employee.getVehicle_type());
+				PreparedStatement preparedStatement = connection.prepareStatement(INSERT_BOOK_SQL)) {
+			preparedStatement.setString(1, customer.getName());
+			preparedStatement.setString(2, customer.getPhoneno());
+			preparedStatement.setString(3, customer.getPickup());
+			preparedStatement.setString(4, customer.getDrop());
+			preparedStatement.setString(5, customer.getVehicle_type());
 			
 			
 
@@ -69,53 +36,6 @@ public class Ambulance_AvailDao {
 		}
 		return result;
 	}
-//	public Register registerAvailDetail(Avail employee) throws ClassNotFoundException {
-//        Register book = null;
-//        String sql = "SELECT * FROM book WHERE vehicle_type = ?";
-//       // int result1 = 0;
-//        //connect();
-//        try (Connection connection = JDBCUtils.getConnection();
-//				// Step 2:Create a statement using connection object
-//				PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-//			preparedStatement.setString(1, employee.getVehicle_type());
-//         
-//        
-////         ResultSet resultSet = preparedStatement.executeQuery();
-////         
-////        if (resultSet.next()) {
-////            String title = resultSet.getString("firstName");
-////            String author = resultSet.getString("phoneno");
-////            String price = resultSet.getString("vehicleno");
-////             
-////            book = new Register( title, author, price);
-////        }
-////         
-////        resultSet.close();
-////        preparedStatement.close();
-////         
-////        return book;
-////    }
-//        System.out.println(preparedStatement);
-//		// Step 3: Execute the query or update query
-//        ResultSet resultSet = preparedStatement.executeQuery();
-//        if (resultSet.next()) {
-//          String title = resultSet.getString("firstName");
-//          String author = resultSet.getString("phoneno");
-//          String price = resultSet.getString("vehicleno");
-//           
-//          book = new Register();
-//          book.setFirstName(title);
-//          book.setPhoneno(author);
-//          book.setVehicleno(price);
-//      }
-//       
-//
-//	} catch (SQLException e) {
-//		// process sql exception
-//		JDBCUtils.printSQLException(e);
-//	}
-//	return book;
-//	
-//}
+
 }
 
